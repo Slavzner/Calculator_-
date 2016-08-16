89c121f2249bedba27c7030a6c42fe1d153fe7b0
Slavzner <Slavapv2007@gmail.com>
2016-08-16 23:04:20 +0300

    add new method getConvertion

--- Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
+++ Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LibraryCalcAlgorithm
 {
     public class CalcAlgorithm
     {
-//-------the main methods---------------------------		
+//-------the main methods---------------------------
+
 		//the main method that runs the algorithm
 		public double Calculate(string input)
 		{
+			string output = getConvertion (input);
+			double result = getResult (output);
+			return result;
+		}
+
+		private string getConvertion(string input)
+		{
+			string output = null;
+			Stack <char> opStack = new Stack <char> ();
+
 
 		}
 
 
 //-------the secondary methods---------------------------		
-
     }
 }
