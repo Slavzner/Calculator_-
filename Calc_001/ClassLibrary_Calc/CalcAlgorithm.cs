acfec58b2e17536a0c35f5ec09354e958a4b1b1d
Slavzner <Slavapv2007@gmail.com>
2016-08-16 22:25:17 +0300

    add a comment

--- Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
+++ Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
@@ -4,10 +4,13 @@ namespace LibraryCalcAlgorithm
 {
     public class CalcAlgorithm
     {
+		//the main method that runs the algorithm
 		public double Calculate(string input)
 		{
 
 		}
+
+
 
     }
 }
