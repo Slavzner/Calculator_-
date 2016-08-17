b1c2603d9f3705031465c99dc791b78a1d78c176
Slavzner <slavapv2007@gmail.com>
2016-08-17 09:47:21 +0300

    refactoring

--- Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
+++ Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
@@ -1,36 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LibraryCalcAlgorithm
 {
     /// <summary>
     /// Library Class of calculation algorithm
     /// </summary>
-    public class CalcAlgorithm: ICalcAlgorithm;
+    public class CalcAlgorithm 
     {
-//-------the main methods---------------------------
+        //-------the main methods---------------------------
 
-		///the main method that runs the algorithm
-        
-		public double Calculate(string input)
-		{
-			string output = getConvertion (input);
-			return getResult (output);
-		}
+        ///the main method that runs the algorithm
+        /// <summary>
+        /// the main method that runs the algorithm
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public double Calculate(string input)
+        {
+            string output = getConvertion(input);
+            return getResult(output);
+        }
 
-/// <summary>
-/// 
-/// </summary>
-/// <param name="input"></param>
-/// <returns></returns>
-		private string getConvertion(string input)
-		{
-			string output = null;
-			Stack <char> opStack = new Stack <char> ();
+        /// <summary>
+        /// method processing of input data
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private string getConvertion(string input)
+        {
+            string output = null;
+            Stack<char> opStack = new Stack<char>();
+        }
 
+        /// <summary>
+        /// method return result of calculation
+        /// </summary>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        private double getResult(string output)
+        {
+        }
 
-		}
-
-
-//-------the secondary methods---------------------------		
+        //-------the secondary methods---------------------------
     }
 }
