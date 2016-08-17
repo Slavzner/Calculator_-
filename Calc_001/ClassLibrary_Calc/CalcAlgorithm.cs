b48b0136606d1fc05a6a6dc6e803d32d2e6f469b
Slavzner <slavapv2007@gmail.com>
2016-08-17 09:08:38 +0300

    add Interfece

--- Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
+++ Calc_001/ClassLibrary_Calc/CalcAlgorithm.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace LibraryCalcAlgorithm
 {
-    public class CalcAlgorithm
+    /// <summary>
+    /// Library Class of calculation algorithm
+    /// </summary>
+    public class CalcAlgorithm: ICalcAlgorithm;
     {
 //-------the main methods---------------------------
 
-		//the main method that runs the algorithm
+		///the main method that runs the algorithm
+        
 		public double Calculate(string input)
 		{
 			string output = getConvertion (input);
 			return getResult (output);
 		}
 
-//
+/// <summary>
+/// 
+/// </summary>
+/// <param name="input"></param>
+/// <returns></returns>
 		private string getConvertion(string input)
 		{
 			string output = null;
---
+++ Calc_001/ClassLibrary_Calc/ICalcAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary_Calc
+{
+    public interface ICalcAlgorithm
+    {
+        //-------the main methods---------------------------
+
+        //the main method that runs the algorithm
+        
+         ///   
+         double Calculate(string input);
+        
+        /// <summary>
+        /// method processing of input data 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        string getConvertion(string input);
+
+    }
+}
